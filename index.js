8b38e852b9d885ff124fb021faf7c080fb32b907
xiaofei <122727418+xiaozhou26@users.noreply.github.com>
2024-04-10 18:15:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const axios = require('axios');
 
 app.use(
     "/",
@@ -12,5 +14,22 @@ app.use(
         logLevel: "error"
     })
 );
+
+function keepalive() {
+    let glitch_app_url = `http://localhost:${port}`;
+
+    axios.get(glitch_app_url)
+        .then(response => {
+            if (response.data.indexOf("./app.js") == -1) {
+                console.log('Server is not running. Restarting...');
+                // Restart server here
+            }
+        })
+        .catch(error => {
+            console.log("Error: " + error);
+        });
+}
+
+setInterval(keepalive, 9 * 1000);
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
